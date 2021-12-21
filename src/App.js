439d965749eabb7b264cafe56328646667cb8069
Jiseon-Park <asdsun07@gmail.com>
2021-12-21 23:15:42 +0900

    20211221

--- src/App.js
+++ src/App.js
@@ -1,51 +1,49 @@
 import React from 'react';
-import propTypes from 'prop-types';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-const foodLike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Malatang',
-    image: 'https://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Korean Fried Chicken',
-    image: 'https://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating: 4.8,
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
   }
-];
 
-function App() {
-  return (
-    <div> 
-      {foodLike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-        ))} 
-    </div>
-  );
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({ 
+      count: current.count + 1,
+    }));
+  };
+
+  minus = () => {
+    this.setState(current => ({ 
+      count: current.count -1 
+    }));
+  };
+
+  componentDidMount() {
+    console.log('component rendered');
+  };
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  };
+
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1> 
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number,
-};
 
 export default App;
